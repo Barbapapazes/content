9b8da4b811aacbe2692a605c0e7c6bbfa16494cc
barbapapazes <esteban.soubiran@insa-cvl.fr>
2024-01-31 10:18:57 +0100

    fix: remove document driven playground

--- playground/document-driven/composables/useTheme.ts
+++
@@ -1,9 +0,0 @@
-import { useContent } from '#imports'
-
-export function useTheme() {
-  const { globals } = useContent()
-
-  const theme = computed(() => globals.value?.theme)
-
-  return theme
-}
